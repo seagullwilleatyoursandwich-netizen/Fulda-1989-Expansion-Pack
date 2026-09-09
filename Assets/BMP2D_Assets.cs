20c5898751f80d50e7945de88f274d9e9599aab1
ZironTheDerg <seagullwilleatyoursandwich@gmail.com>
2026-09-09 15:41:06 +0100

    V1.2.0

---
+++ Assets/BMP2D_Assets.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using MelonLoader;
+using MelonLoader.Utils;
+using System.IO;
+using ModUtil;
+
+namespace Fulda1989
+{
+    public class BMP2D_Assets : Module
+    {
+        internal static GameObject bmp2d_full;
+
+        public override void LoadStaticAssets()
+        {
+            string path = Path.Combine(MelonEnvironment.ModsDirectory + "/Fulda1989", "bmp2d_full");
+
+            AssetBundle bundle = AssetBundle.LoadFromFile(path);
+
+            if (bundle == null)
+            {
+                MelonLogger.Error("FAILED TO LOAD BMP-2D ASSETBUNDLE");
+                return;
+            }
+
+            bmp2d_full = bundle.LoadAsset<GameObject>("BMP2Dfull");
+
+            if (bmp2d_full == null)
+            {
+                MelonLogger.Error("FAILED TO FIND bmp2d_full");
+                return;
+            }
+        }
+    }
+}
---
+++ Assets/M60_Assets.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using MelonLoader;
+using MelonLoader.Utils;
+using System.IO;
+using ModUtil;
+
+namespace Fulda1989
+{
+    public class M60_Assets : Module
+    {
+        internal static GameObject m60_full;
+
+        public override void LoadStaticAssets()
+        {
+            string path = Path.Combine(MelonEnvironment.ModsDirectory + "/Fulda1989", "m60_full");
+
+            AssetBundle bundle = AssetBundle.LoadFromFile(path);
+
+            if (bundle == null)
+            {
+                MelonLogger.Error("FAILED TO LOAD M60 ERA ASSETBUNDLE");
+                return;
+            }
+
+            m60_full = bundle.LoadAsset<GameObject>("m60_full");
+
+            if (m60_full == null)
+            {
+                MelonLogger.Error("FAILED TO FIND m60_full");
+                return;
+            }
+        }
+    }
+}
